294e8ca265cbb4e3e4d4be6e60a9c2352412bc75
Minecraft User <rogerk@pvp-minecraft.com>
2016-08-28 14:14:29 -0500

    Compiling

--- src/main/java/com/steamcraftmc/EssentiallyMisc/MainPlugin.java
+++ src/main/java/com/steamcraftmc/EssentiallyMisc/MainPlugin.java
@@ -11,8 +11,6 @@ public class MainPlugin extends JavaPlugin {
 	public Boolean _exLogging;
 	public final MainConfig Config;
 	public com.steamcraftmc.EssentiallyMisc.Commands.CmdAfk afk;
-	public com.steamcraftmc.EssentiallyMisc.Commands.CmdGod god;
-	public com.steamcraftmc.EssentiallyMisc.Commands.CmdFireball fb;
 
 	public MainPlugin() {
 		_exLogging = true;
@@ -30,36 +28,21 @@ public class MainPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        new com.steamcraftmc.EssentiallyMisc.Commands.CmdFeed(this);
-        new com.steamcraftmc.EssentiallyMisc.Commands.CmdHeal(this);
-        new com.steamcraftmc.EssentiallyMisc.Commands.CmdFixLight(this);
-        new com.steamcraftmc.EssentiallyMisc.Commands.CmdFly(this);
-        new com.steamcraftmc.EssentiallyMisc.Commands.CmdGameMode(this);
         new com.steamcraftmc.EssentiallyMisc.Commands.CmdSetText(this);
         afk = new com.steamcraftmc.EssentiallyMisc.Commands.CmdAfk(this);
-        new com.steamcraftmc.EssentiallyMisc.Commands.CmdBurn(this);
-        new com.steamcraftmc.EssentiallyMisc.Commands.CmdLightning(this);
-        fb = new com.steamcraftmc.EssentiallyMisc.Commands.CmdFireball(this);
         new com.steamcraftmc.EssentiallyMisc.Commands.CmdHat(this);
-        new com.steamcraftmc.EssentiallyMisc.Commands.CmdGC(this);
-        god = new com.steamcraftmc.EssentiallyMisc.Commands.CmdGod(this);
         new com.steamcraftmc.EssentiallyMisc.Commands.CmdNear(this);
-        new com.steamcraftmc.EssentiallyMisc.Commands.CmdRepair(this);
         new com.steamcraftmc.EssentiallyMisc.Commands.CmdSuicide(this);
                 
     	_listener = new WorldEvents(this);
         getServer().getPluginManager().registerEvents(_listener, this);
         afk.start();
-        fb.start();
-        god.start();
         log(Level.INFO, "Plugin listening for events.");
     }
 
     @Override
     public void onDisable() {
         afk.stop();
-        fb.stop();
-        god.stop();
     	HandlerList.unregisterAll(_listener);
     }
 
